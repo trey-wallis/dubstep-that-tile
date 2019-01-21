2348ebca64993a5b2ced2ae080b3d598cba5a045
Trey Wallis <mrwallis36@hotmail.com>
2019-01-21 02:20:45 -0700

    Added login and register enus

--- src/react/menu/Login.js
+++ src/react/menu/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './custom.css';
 
 class Login extends Component {
 
@@ -6,8 +7,21 @@ class Login extends Component {
 		const {ui, domain} = this.props;
 
 		return(
-		<div className="Login h-100 relative flex justify-center">
-			<button className="absolute bottom-2 bg-color--empty outline--none bw0 dim f3" onClick={ () => {ui.setRoute("title")} } >Back</button>
+		<div className="Login">
+			{ui.loginHeader}
+			{ui.title}
+			<div className="f3-ns f4 tc mt4">Login</div>
+				<div className="Login__form flex flex-wrap justify-center mt2">
+					<div className="b">Username</div>
+					<input className="outlne--none bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="text" value="Username" />
+					<div className="b">Password</div>
+					<input className="bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="password" value="Password" />
+					<div className="b">Repeat Password</div>
+					<input className="bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="password" value="Repeat Password" />
+					<button className="bg-color--inherit bw0 outline--none f4-ns f5 b mt-2 dim">Submit</button>
+				</div>
+			<div className="Login__request red tc mt2">*Invalid username or password</div>
+			{ui.backButton}
 		</div>
 		);
 	}
