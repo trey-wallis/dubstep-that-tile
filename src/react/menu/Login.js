54df05de85d09b4bd59fd53de96bb8f3c0a1f1ed
jfr314 <jamesfreis314@gmail.com>
2019-01-21 16:20:03 -0800

    Removed repeat password from login, changed values to placeholders, hide invalid message on load

--- src/react/menu/Login.js
+++ src/react/menu/Login.js
@@ -13,14 +13,12 @@ class Login extends Component {
 			<div className="f3-ns f4 tc mt4">Login</div>
 				<div className="Login__form flex flex-wrap justify-center mt2">
 					<div className="b">Username</div>
-					<input className="outlne--none bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="text" value="Username" />
+					<input className="outlne--none bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="text" placeholder="Username" />
 					<div className="b">Password</div>
-					<input className="bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="password" value="Password" />
-					<div className="b">Repeat Password</div>
-					<input className="bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="password" value="Repeat Password" />
+					<input className="bg-color--black-t1 bw0 outline--none f4-ns f5 mb3" type="password" placeholder="Password" />
 					<button className="bg-color--inherit bw0 outline--none f4-ns f5 b mt-2 dim">Submit</button>
 				</div>
-			<div className="Login__request red tc mt2">*Invalid username or password</div>
+			<div className="Login__request red tc mt2 hide">*Invalid username or password</div>
 			{ui.backButton}
 		</div>
 		);
